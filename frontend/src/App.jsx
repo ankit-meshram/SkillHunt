0ab714f4119c719f6e029d2e00b99f4815e43d3d
sank-8 <sanket.unifins@gmail.com>
2025-07-30 22:03:44 +0530

    home page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Login from './components/auth/Login.jsx';
-import Signup from './components/auth/Signup.jsx/index.js';
-import Home from './components/Home.jsx/index.js';
-import Jobs from './components/Jobs.jsx/index.js';
-import Browse from './components/Browse.jsx/index.js';
-import Profile from './components/Profile.jsx/index.js';
-import JobDescription from './components/JobDescription.jsx/index.js';
-import Companies from './components/admin/Companies.jsx';
-import CompanyCreate from './components/admin/CompanyCreate.jsx';
-import CompanySetup from './components/admin/CompanySetup.jsx';
-import AdminJobs from "./components/admin/AdminJobs.jsx";
-import PostJob from './components/admin/PostJob.jsx';
-import Applicants from './components/admin/Applicants.jsx';
-import ProtectedRoute from './components/admin/ProtectedRoute.jsx';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
+import Home from './components/Home';
+import Jobs from './components/Jobs';
+import Browse from './components/Browse';
+import Profile from './components/Profile';
+import JobDescription from './components/JobDescription';
+import Companies from './components/admin/Companies';
+import CompanyCreate from './components/admin/CompanyCreate';
+import CompanySetup from './components/admin/CompanySetup';
+import AdminJobs from "./components/admin/AdminJobs";
+import PostJob from './components/admin/PostJob';
+import Applicants from './components/admin/Applicants';
+import ProtectedRoute from './components/admin/ProtectedRoute';
 
 
 const appRouter = createBrowserRouter([
