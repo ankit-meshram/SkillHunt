65a6028201ffb89a1affeb57a42526aa18416a46
sank-8 <sanket.unifins@gmail.com>
2025-07-30 22:51:23 +0530

    protected route

--- frontend/src/components/admin/ProtectedRoute.jsx
+++ frontend/src/components/admin/ProtectedRoute.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children }) => {
-    return (
-        <div>
-            {children}
-        </div>
-    );
+const user = {
+    role: "recruiter"
 };
 
+const ProtectedRoute = ({ children }) => {
+
+
+    useEffect(() => {
+        if (user === null || user.role !== 'recruiter') {
+            navigate("/");
+        }
+    }, []);
+
+    return (
+        <>
+            {children}
+        </>
+    );
+};
 export default ProtectedRoute;
